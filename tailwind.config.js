3ff2ab770cb4f4a6971c6422d15898fc8b16f87d
LukeGiles3 <luke.giles333@gmail.com>
2022-08-16 16:42:37 -0600

    navBar, site layout, color scheme, fontAwesome

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  theme: {
+    colors: {
+      'beige': '#BF9D73',
+      'blue': '#2C3D73',
+      'grey': '#7D828C',
+      'green': '#3A591B',
+      'red': '#8C3C2A',
+      'white': '#F2F2F2',
+      'black': '#0D0D0D'
+    }
+  }
+}
